0499f7318a6c86ae41af9e93a94968659df17c26
rapaduragames <antilacus@gmail.com>
2026-04-15 15:08:21 -0300

    questão 2: maior,some,média dos números fornecidos.

--- src/questao02/Questao02.java
+++ src/questao02/Questao02.java
@@ -1,4 +1,36 @@
 package questao02;
 
+import java.util.*;
+
 public class Questao02 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] numeros = new int[10];
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Digite o número " + (i + 1));
+            numeros[i] = sc.nextInt();
+        }
+        
+        int maiorN = 0;
+        for (int n : numeros) {
+            if (n > maiorN) {
+                maiorN = n;
+            }
+        }
+
+        int soma = 0;;
+        for (int i : numeros) {
+            soma = soma + i;
+        }
+
+        int media = soma / 10;
+
+        System.out.println("Maior número:" + maiorN);
+        System.out.println("Some:" + soma);
+        System.out.println("média:" + media);
+
+    }
 }

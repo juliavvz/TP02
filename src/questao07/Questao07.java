3a13a849c549367c52e05864f64ccb26e1fb943c
Júlia <juliavvitoria376@gmail.com>
2026-04-15 14:46:14 -0300

    Adiciona questao 7: multiplicacao de matriz 3x4 por constante

--- src/questao07/Questao07.java
+++ src/questao07/Questao07.java
@@ -1,8 +1,37 @@
 package questao07;
+import java.util.Scanner;
 
 public class Questao07 {
-
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        
+        int[][] matriz = new int[3][4];
+        
+        System.out.println("Digite 12 valores para matriz 3x4:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print("Elemento [" + i + "][" + j + "]: ");
+                matriz[i][j] = scanner.nextInt();
+            }
+        }
+        
+        System.out.print("Digite a constante multiplicativa: ");
+        int constante = scanner.nextInt();
+        
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                matriz[i][j] *= constante;
+            }
+        }
+        
+        System.out.println("Matriz multiplicada:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+        scanner.close();
     }
 }
